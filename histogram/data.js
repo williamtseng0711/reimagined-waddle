cda74d4f9619125f0f26f83bb8a17ecbced8b8c2
williamtseng0711 <ttwycy@gmail.com>
2023-09-08 14:05:25 +0800

    add histogram & pie project

---
+++ histogram/data.js
@@ -0,0 +1,12 @@
+d3.csv('https://raw.githubusercontent.com/ryanchung403/dataset/main/train_data_titanic.csv').then(
+    res=>{
+        console.log('CSV:',res)
+    }
+);
+
+let set1=[
+    1,2,2,3,3,3,4,4,5
+];
+let set2=[
+    2,3,3,4,4,4,5,5,6
+];
